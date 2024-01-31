b06ea219be09637225837a0eec198fe81cec71a6
Zorvep <jacobwebserter2019@gmail.com>
2024-01-30 17:39:33 -0700

    w

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    function autoResize(textarea) {
-        textarea.style.height = 'auto';
-        textarea.style.height = textarea.scrollHeight + 'px';
-    }
+function autoResize(textarea) {
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 'px';
+}
 
-    var textareas = document.getElementsByClassName('hackerbox');
-    for (var i = 0; i < textareas.length; i++) {
-        autoResize(textareas[i]);
-    }
-});
+var textareas = document.getElementsByClassName('hackerbox');
+for (var i = 0; i < textareas.length; i++) {
+    autoResize(textareas[i]);
+}
