ff5052c84146ddcbe71bb0b78e582d0bc92dc831
Zorvep <jacobwebserter2019@gmail.com>
2024-01-30 17:33:08 -0700

    w

---
+++ main.js
@@ -0,0 +1,9 @@
+function autoResize(textarea) {
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 'px';
+}
+
+var textareas = document.getElementsByClassName('hackerbox');
+for (var i = 0; i < textareas.length; i++) {
+    autoResize(textareas[i]);
+}
