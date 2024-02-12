7286fc149d21bac6d933ef8d76667a749d706a72
Jacob <jacobwebster2019@gmail.com>
2024-02-12 11:43:09 -0700

    e

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-function ToggleSideBar() {
-    var element = document.getElementById("sidebar")
-    var display = element.style.display
-    console.log(element)
+function ToggleSideBar(triggeringElement) {
+    let element = document.getElementById("sidebar")
+    let elements = document.getElementById("sidebarbutton")
+    let display = element.style.display
+
+    triggeringElement.style.display = "none"
+
+    for (let i = 0; elements.length; i++) {
+        if (elements[i] !== triggeringElement) {
+            elements[i].style.display = "inline"
+        }
+    }
 
     if (display !== "none") {
         element.style.display = "none"
