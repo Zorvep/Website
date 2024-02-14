a81d63ece330d8ac5281e5b3515f4e7e842c7839
Jacob <jacobwebster2019@gmail.com>
2024-02-14 11:59:52 -0700

    e

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-function ToggleSideBar(triggeringElement) {
+function ToggleSideBar() {
+    let sidebar = document.getElementById("sidebar")
+
     if (sidebar.style.display !== "none") {
         sidebar.style.display = "none"
     }
