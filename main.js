e2b02fea57077a3f163c9cdcebbfc72aeff6e7f5
Zorvep <jacobwebserter2019@gmail.com>
2024-05-07 08:22:57 -0700

    e

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ OnMouseMove = (mouse) => {
 
     CursorGlow.style.left = (mouse.clientX - CursorGlow.offsetWidth/2) + "px";
     CursorGlow.style.top = (mouse.clientY - CursorGlow.offsetHeight/2) + "px";
-    console.log(`width: ${CursorGlow.width} px`);
 };
 
 addEventListener("mousemove", (OnMouseMove));
