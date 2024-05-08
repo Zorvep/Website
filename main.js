652024c1ac3e9b04f269428c469d2530c716eab1
Zorvep <jacobwebserter2019@gmail.com>
2024-05-08 14:36:22 -0700

    e

--- main.js
+++ main.js
@@ -2,9 +2,16 @@
 
 OnMouseMove = (mouse) => {
     const CursorGlow = document.getElementById("CursorGlow");
+    const element = document.documentElement;
 
-    CursorGlow.style.left = (mouse.clientX - CursorGlow.offsetWidth/2) + "px";
-    CursorGlow.style.top = (mouse.clientY - CursorGlow.offsetHeight/2) + "px";
+    var scrollTop = element.scrollTop;
+    var scrollHeight = element.scrollHeight;
+    var scrollPercent = (scrollTop / scrollHeight);
+
+    CursorGlow.style.left = ((mouse.clientX) - CursorGlow.offsetWidth/2) + "px";
+    CursorGlow.style.top = ((mouse.clientY +scrollTop) - CursorGlow.offsetHeight/2) + "px";
+    console.log(scrollTop);
 };
 
 addEventListener("mousemove", (OnMouseMove));
+addEventListener("scroll", (OnMouseMove));
