c063459781ca1742d17ee662fcf532eec61e69d3
Zorvep <jacobwebserter2019@gmail.com>
2024-10-17 13:20:08 -0700

    e

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
-SetCursorPosition = (mouse) => {
-    const CursorGlow = document.getElementById("CursorGlow");
-    const element = document.documentElement;
-    const scrollTop = element.scrollTop;
-    const scrollLeft = element.scrollLeft;
+window.onload = () => {
+    const cursor = document.getElementById("Cursor");
+    let clientX = 0;
+    let clientY = 0;
+    let ticking = false;
 
-    CursorGlow.style.left = ((mouse.clientX + scrollLeft) - CursorGlow.offsetWidth/2) + "px";
-    CursorGlow.style.top = ((mouse.clientY + scrollTop) - CursorGlow.offsetHeight) + "px";
-    console.log(scrollTop);
+    const updateCursor = () => {
+        cursor.style.display = 'block';
+        cursor.style.top = clientY + window.scrollY + "px";
+
+        cursor.animate({
+            left: clientX + window.scrollX + "px",
+            top: clientY + window.scrollY + "px"
+
+        }, {duration: 300, fill: "forwards"});
+    };
+
+    document.addEventListener("mousemove", (e) => {
+        clientX = e.clientX;
+        clientY = e.clientY;
+        updateCursor();
+    });
+
+    document.addEventListener("scroll", () => {
+        if (!ticking) {
+            window.requestAnimationFrame(() => {
+                updateCursor();
+                ticking = false;
+            });
+            ticking = true;
+        }
+    });
+
+    document.addEventListener('mouseout', (e) => {
+        if (!e.relatedTarget) {
+            cursor.style.display = 'none';
+        }
+    });
+
+    document.addEventListener('mouseover', (e) => {
+        if (!e.relatedTarget) {
+            cursor.style.display = 'block';
+        }
+    });
 };
-
-addEventListener("mousemove", (SetCursorPosition));
-addEventListener("scroll", (SetCursorPosition));
